ad4a50352017fda464784d752eab77529d0ef4ea
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-10 17:25:35 +0700

    feat: add new feature

--- src/Components/CobaState.tsx
+++ src/Components/CobaState.tsx
@@ -7,18 +7,25 @@ export const CobaState = () => {
       "Gas Belajar",
       "Sudah Paham?",
       "Ayo aja gw mah",
+      "Yeyyy merdeka",
+      "17 Agustus 2023",
+      "Di rumah",
+      "Sedang Sibuk",
    ];
 
    const [greet, setGreet] = useState(0);
+   const [add, setAdd] = useState("");
 
    const changeGreet = () => {
-      setGreet(() => Math.floor(Math.random() * 4));
+      setGreet(() => Math.floor(Math.random() * 8));
    };
 
    return (
       <>
          <h2>{randomQuote[greet]}</h2>
          <Buttons click={changeGreet} task={"ubah"} />
+         <input type='text' />
+         <Buttons click={} />
       </>
    );
 };
